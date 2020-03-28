876c8c505de9cd1a410344accded073538db79ed
Ericka Koyama <ericka.koyama@gmail.com>
2020-03-28 15:47:37 -0700

    Add computeSignalHeight and computeSignalWidth and some temp mock 2d array data

--- src/com/company/Assig4.java
+++ src/com/company/Assig4.java
@@ -68,6 +68,59 @@ class DataMatrix implements BarcodeIO {
       return true;
    }
 
+   private boolean[][] getMockImageGrid() {  // TODO: TEMP DO NOT SUBMIT THIS!!!!!
+      boolean[][] mockImageData = new boolean[][]{ // 6 x 20 grid, signal is already pushed to bottom-left
+            {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+            {true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false},
+            {true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+            {true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, false},
+            {true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+            {true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, false},
+      };
+
+      return mockImageData;
+   }
+
+   /**
+    * Compute the height of the signal.
+    * Pre-condition: Signal is placed at bottom-left of image.
+    * @return Height of signal.
+    */
+   public int computeSignalHeight() { // TODO: Update this method once we get BarcodeImage data.
+      int signalHeight = 0;
+      boolean[][] mockImage = getMockImageGrid();
+
+      for (int i = 0; i < mockImage.length; i++) { // should be i < image.MAX_HEIGHT
+         if (mockImage[i][0]) { // if the square is black, it's the top of the spine
+            signalHeight = (mockImage.length - i) - 2; // subtract top and bottom spine from signal height
+            break;
+         }
+      }
+
+      return signalHeight;
+   }
+
+   /**
+    * Compute the width of the signal.
+    * Pre-condition: Signal is placed at bottom-left of image.
+    * @return Width of signal.
+    */
+   public int computeSignalWidth() { // TODO: Update this method once we get BarcodeImage data.
+      int signalWidth = 0;
+      boolean[][] mockImage = getMockImageGrid();
+
+      boolean[] bottomRow = mockImage[mockImage.length - 1];
+
+      for (boolean cell: bottomRow) {
+         if (cell) {
+            signalWidth++;
+         } else {
+            break;
+         }
+      }
+
+      return signalWidth - 2; // subtract right and left spine
+   }
 
    public boolean translateImageToText() {
       return true;
