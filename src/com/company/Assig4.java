c3a78239a5eafaa3eef6388781a64e95131ba6dd
Ericka Koyama <ericka.koyama@gmail.com>
2020-03-28 08:58:36 -0700

    initial commit

---
+++ src/com/company/Assig4.java
@@ -0,0 +1,66 @@
+package com.company;
+
+/**
+ * Project Members: Ericka Koyama, Holly Stephens, Ngoc Tran Do
+ * CST 338 Software Design
+ * Assignment 4 - Barcode Scanner
+ */
+
+
+public class Assig4 implements {
+
+   public static void main(String[] args) {
+      // write your code here
+   }
+}
+
+/**
+ *
+ */
+interface BarcodeIO {
+   public boolean scan(BarcodeImage bc);
+
+   public boolean readText(String text);
+
+   public boolean generateImageFromText();
+
+   public boolean translateImageToText();
+
+   public void displayTextToConsole();
+
+   public void displayImageToConsole();
+}
+
+/**
+ *
+ */
+class BarcodeImage implements Cloneable {
+}
+
+/**
+ *
+ */
+class DataMatrix implements BarcodeIO {
+   public boolean scan(BarcodeImage bc) {
+   }
+
+
+   public boolean readText(String text) {
+   }
+
+
+   public boolean generateImageFromText() {
+   }
+
+
+   public boolean translateImageToText() {
+   }
+
+
+   public void displayTextToConsole() {
+   }
+
+
+   public void displayImageToConsole() {
+   }
+}
